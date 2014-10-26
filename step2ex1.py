e440c1b29c6f736ea8b97696210774352ebc26d9
zlt298 <zealot298@hotmail.com>
2014-10-25 17:06:46 -1000

    Step 2 added
    Added 1D Convection:
    Inviscid Burgers

--- step2ex1.py
+++ step2ex1.py
@@ -6,13 +6,11 @@ import time, sys
 linear convection u = u0 = 2 for interval 0.5<=x<=1 and 1 everywhere else
 """
 
-nx = 161
-nt = 25
+nx = 21
+nt = 20
 
 dx = 2./(nx-1) #dx = 0.05
-dt = 0.0125
-
-c = 1.
+dt = 0.025
 
 u = np.ones(nx)
 u[.5/dx : 1/dx + 1] = 2
@@ -33,7 +31,7 @@ un = np.ones(nx) #initialize a temporary array
 for n in range(nt):  #loop for values of n from 0 to nt, so it will run nt times
     un = u.copy() ##copy the existing values of u into un
     for i in range(1,nx): ## iterate through x position...
-        u[i] = un[i]-c*dt/dx*(un[i]-un[i-1])
+        u[i] = un[i]-un[i]*dt/dx*(un[i]-un[i-1])
 plt.plot(np.linspace(0,2,nx), u)
 plt.show()
 print u
